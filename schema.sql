fcc7770e2fc8519a091bc25b243c2611c32110c5
clarencepenz <clarencepenz@users.noreply.github.com>
2020-05-30 19:41:10 +0100

    schema implementation

---
+++ schema.sql
@@ -0,0 +1,51 @@
+CREATE TABLE articles (
+    pid serial NOT NULL,
+    title character(255),
+    body character(255),
+    author character(255),
+    author_id integer,
+    url character(255),
+    date timestamp(4) with time zone,
+    id integer NOT NULL,
+    CONSTRAINT articles_pkey PRIMARY KEY (pid)
+);
+
+
+CREATE TABLE gifs (
+    gid serial NOT NULL ,
+    title character(255),
+    author character(255),
+    author_id integer,
+    avatar text,
+    url character(255),
+    date timestamp(4) with time zone,
+    id integer,
+    CONSTRAINT gifs_pkey PRIMARY KEY (gid)
+);
+
+CREATE TABLE comments (
+    cid serial NOT NULL,
+    comment character(255),
+    post_id integer,
+    gif_id integer,
+    author character(255),
+    author_id integer,
+    avatar character(255),
+    date timestamp(4) with time zone,
+    id integer,
+    CONSTRAINT comments_pkey PRIMARY KEY (cid)
+);
+
+
+create TABLE users (
+    uid serial NOT NULL,
+    username character(255),
+    email character(255),
+    password text,
+    about character(255),
+    twitter character(255),
+    github character(255),
+    facebook character(255),
+    url character(255),
+    CONSTRAINT users_pkey PRIMARY KEY (uid)
+);
