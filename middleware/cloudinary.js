65d68f1ffb4a6369ed3f02077e74b3c0016a4dc8
clarencepenz <clarencepenz@users.noreply.github.com>
2020-05-31 15:58:53 +0100

    settings updated

--- middleware/cloudinary.js
+++ middleware/cloudinary.js
@@ -4,9 +4,9 @@ dotenv.config();
 
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'huwj1ufzc',
-    api_key: process.env.CLOUDINARY_API_KEY || '413813264741747',
-  api_secret: process.env.CLOUDINARY_API_SECRET || 'iLTaIcuRVRQdpW2EmAieWviWmBs',
+    cloud_name: process.env.CLOUD_NAME || 'huwj1ufzc',
+    api_key: process.env.API_KEY || '413813264741747',
+  api_secret: process.env.API_SECRET || 'iLTaIcuRVRQdpW2EmAieWviWmBs',
     cloudinary_url: process.env.CLOUDINARY_URL
 });
 module.exports = cloudinary;
